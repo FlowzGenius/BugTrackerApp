9fb8a6443ad720cde5569835167050d8e7d8035f
flowzGenius <217010298@stud.cut.ac.za>
2020-01-06 11:02:32 +0200

    Added Data access library

---
+++ BugTrackeData.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,42 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BugTrackeData.Library.Internal.DataAccess
+{
+    public class SqlDataAccess
+    {
+        private string GetConnecctionString(string connectionStringName)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+
+            return connectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnecctionString(connectionStringName);
+
+            using(IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                var output = cnn.Query<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure).AsList();
+
+                return output;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parametres, string connectionStringName)
+        {
+            string connectionString = GetConnecctionString(connectionStringName);
+
+            using(IDbConnection cnn = new SqlConnection(connectionString))
+            {
+                cnn.Execute(storedProcedure, parametres, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
